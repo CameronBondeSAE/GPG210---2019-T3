a7aead182c19c58cb33f297d5a77c29b453c1c31
Luca <lucatannler@hotmail.com>
2019-10-17 23:37:56 +1100

    Car Improvements
    + Some improvements on the car motion (Still in work)
    + Added Car/Wheel script to Student.Luca namespace

--- Assets/Students/Luca/Car.cs
+++ Assets/Students/Luca/Car.cs
@@ -1,82 +1,129 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Students.Luca;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Car : MonoBehaviour
+namespace Students.Luca
 {
-    public List<Wheel> steeringWheels;
-    public List<Wheel> driveWheels;
+    public class Car : MonoBehaviour
+    {
+        public Rigidbody rb;
+        
+        [Header("Car Settings")]
+        public List<Wheel> steeringWheels;
+        public List<Wheel> driveWheels;
 
-    public float motorStrength;
-    public Rigidbody rb;
+        public float motorStrength;
+        
+        [Header("Float Settings")]
+        public float currentDistanceToGround = 0;
+        public float zeroForceHeight = 3;
+        public AnimationCurve forceHeightCurve;
+        public Vector3 maxForce;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
         
-        if(steeringWheels == null)
-            steeringWheels = new List<Wheel>();
-        if(driveWheels == null)
-            driveWheels = new List<Wheel>();
+            if(steeringWheels == null)
+                steeringWheels = new List<Wheel>();
+            if(driveWheels == null)
+                driveWheels = new List<Wheel>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //HandleFloating();
+            HandleInput();
+        }
+
+        private void HandleFloating()
+        {
+            RaycastHit hit;
+            Debug.DrawRay(transform.position, -currentDistanceToGround * transform.up, Color.blue);
+            if (Physics.Raycast(transform.position, -transform.up, out hit, zeroForceHeight))
+            {
+                currentDistanceToGround = hit.distance;
+            }
+            else
+            {
+                currentDistanceToGround = zeroForceHeight;
+            }
+            float curveValue = Mathf.Clamp(currentDistanceToGround, 0, zeroForceHeight) / zeroForceHeight;
+            Vector3 finalForce = transform.TransformDirection(forceHeightCurve.Evaluate(curveValue) * maxForce);
+        
+            rb.AddForceAtPosition(finalForce, transform.position);
+        }
+
+        private void HandleInput()
+        {
+            // Forward
+            if (Input.GetKey(KeyCode.W))
+            {
+                float wheelForce = motorStrength / driveWheels.Count;
+                foreach (var wheel in driveWheels)
+                {
+                    //wheel.ApplyForce(wheelForce);
+                    // HACK TEST
+                    Vector3 dir = wheel.transform.forward;
+                    dir.y = 0;
+                    rb.AddForceAtPosition(wheelForce * dir, wheel.transform.position);
+                }
+            }
+            
+            // Backward
+            if (Input.GetKey(KeyCode.S))
+            {
+                float wheelForce = motorStrength / driveWheels.Count;
+                foreach (var wheel in driveWheels)
+                {
+                    wheel.ApplyForce(-wheelForce);
+                }
+            }
+
+            // Turn Left
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                foreach (var wheel in steeringWheels)
+                {
+                    wheel.inputTurnLeft = true;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                foreach (var wheel in steeringWheels)
+                {
+                    wheel.inputTurnLeft = false;
+                }
+            }
+        
+            // Turn Right
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                foreach (var wheel in steeringWheels)
+                {
+                    wheel.inputTurnRight = true;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                foreach (var wheel in steeringWheels)
+                {
+                    wheel.inputTurnRight = false;
+                }
+            }
+        }
+
+        public static bool ApproximatelyT(float a, float b, float threshold)
+        {
+            return ((a - b) < 0 ? ((a - b) * -1) : (a - b)) <= threshold;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            float wheelForce = motorStrength / driveWheels.Count;
-            foreach (var wheel in driveWheels)
-            {
-                wheel.ApplyForce(wheelForce);
-            }
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            float wheelForce = motorStrength / driveWheels.Count;
-            foreach (var wheel in driveWheels)
-            {
-                wheel.ApplyForce(-wheelForce);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            foreach (var wheel in steeringWheels)
-            {
-                wheel.inputTurnLeft = true;
-            }
-        }
-
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            foreach (var wheel in steeringWheels)
-            {
-                wheel.inputTurnLeft = false;
-            }
-        }
-        
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            foreach (var wheel in steeringWheels)
-            {
-                wheel.inputTurnRight = true;
-            }
-        }
-
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            foreach (var wheel in steeringWheels)
-            {
-                wheel.inputTurnRight = false;
-            }
-        }
-    }
-
-    public void AddForce(Vector3 force, Vector3 offset)
-    {
-        
-    }
 }
+
--- Assets/Students/Luca/Wheel.cs
+++ Assets/Students/Luca/Wheel.cs
@@ -1,94 +1,180 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Serialization;
 
-public class Wheel : MonoBehaviour
+namespace Students.Luca
 {
-    public static float groundFriction = 0.1f;
-
-    public Car master;
-    
-    //private Rigidbody rb;
-    private Vector3 localBaseRotation;
-
-    public bool inputTurnLeft = false;
-    public bool inputTurnRight = false;
-
-    public float maxTurnAngle = 20; //Degrees
-    public float rotationSpeed = 30;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class Wheel : MonoBehaviour
     {
-        master = GetComponentInParent<Car>();
-        //rb = GetComponentInParent<Rigidbody>(); // Hacky
-        localBaseRotation = transform.localRotation.eulerAngles;
-    }
+        public static float groundFriction = 0.1f;
 
-    public bool doDebug = false;
-    private Vector3 localVelocity = Vector3.zero;
-    // Update is called once per frame
-    void Update()
-    {
-        localVelocity = transform.InverseTransformDirection(master.rb.velocity);
-        localVelocity.y = 0;
-        
-        Vector3 newEulerRot = transform.localRotation.eulerAngles;
-        if (inputTurnLeft)
+        public Car master;
+    
+        //private Rigidbody rb;
+        private Vector3 localBaseRotation;
+
+        public bool inputTurnLeft = false;
+        public bool inputTurnRight = false;
+
+        public float maxTurnAngle = 20; //Degrees
+        public float rotationSpeed = 30;
+
+        public float turnForceMultiplier = 0.5f;
+
+        [Header("Float Settings")]
+        public float distanceToGround = 1f;
+        public float zeroForceHeight = 3;
+        public AnimationCurve forceHeightCurve;
+        public Vector3 maxForce;
+        public float currentDistanceToGround = 0;
+    
+        // Start is called before the first frame update
+        void Start()
         {
-            newEulerRot.y = Mathf.MoveTowardsAngle(newEulerRot.y, localBaseRotation.y-maxTurnAngle, rotationSpeed * Time.deltaTime);
-        }
-        if (inputTurnRight)
-        {
-            newEulerRot.y = Mathf.MoveTowardsAngle(newEulerRot.y, localBaseRotation.y+maxTurnAngle, rotationSpeed * Time.deltaTime);
+            master = GetComponentInParent<Car>();
+            //rb = GetComponentInParent<Rigidbody>(); // Hacky
+            localBaseRotation = transform.localRotation.eulerAngles;
         }
 
-        if (!inputTurnLeft && !inputTurnRight && !Mathf.Approximately(newEulerRot.y, localBaseRotation.y))
+        public bool doDebug = false;
+        private Vector3 localVelocity = Vector3.zero;
+        // Update is called once per frame
+        void Update()
         {
-            newEulerRot.y = Mathf.MoveTowardsAngle(newEulerRot.y, localBaseRotation.y, rotationSpeed * Time.deltaTime);
-        }
-        
-        transform.localRotation = Quaternion.Euler(newEulerRot);
-        
-        
-        
-        // Friction
-        if (localVelocity.magnitude > 0.01f)
-        {
-            /*float angleToVelocity = Vector3.SignedAngle(localVelocity,transform.forward, Vector3.up);
-            Vector3 counterVelocity = Quaternion.AngleAxis(angleToVelocity, Vector3.up) * -localVelocity;
-            master.rb.AddForceAtPosition(counterVelocity, master.transform.position + transform.localPosition);
+            HandleFloating();
+            
+            localVelocity = transform.InverseTransformDirection(master.rb.velocity);
+            //localVelocity.y = 0;
 
-            if (doDebug)
+            bool rotChanged = false; // HACK TODO
+            Vector3 newEulerRot = transform.localRotation.eulerAngles;
+            if (inputTurnLeft)
             {
-                Debug.DrawRay(transform.position,localVelocity, Color.red);
-                Debug.DrawRay(transform.position,transform.forward * 2, Color.green);
-                Debug.DrawRay(transform.position,counterVelocity*10, Color.blue);
+                newEulerRot.y = Mathf.MoveTowardsAngle(newEulerRot.y, localBaseRotation.y-maxTurnAngle, rotationSpeed * Time.deltaTime);
+                rotChanged = true;
+            }
+            if (inputTurnRight)
+            {
+                newEulerRot.y = Mathf.MoveTowardsAngle(newEulerRot.y, localBaseRotation.y+maxTurnAngle, rotationSpeed * Time.deltaTime);
+                rotChanged = true;
+            }
+
+            if (!inputTurnLeft && !inputTurnRight && !Mathf.Approximately(newEulerRot.y, localBaseRotation.y))
+            {
+                newEulerRot.y = Mathf.MoveTowardsAngle(newEulerRot.y, localBaseRotation.y, rotationSpeed * Time.deltaTime);
+                rotChanged = true;
+            }
+
+            if (rotChanged)
+            {
+                transform.localRotation = Quaternion.Euler(newEulerRot);
+            }
+
+
+            if ((!Car.ApproximatelyT(master.rb.velocity.x, 0, 0.01f) ||
+                !Car.ApproximatelyT(master.rb.velocity.z, 0, 0.01f)) && IsGrounded())
+            {
+                //localVelocity.y = 0;
+                
+                master.rb.AddForceAtPosition(master.transform.TransformDirection(-localVelocity)*turnForceMultiplier, transform.position);
+                
+                if (doDebug)
+                {
+                    Vector3 linepos = transform.position;
+                    //linepos.y += 2;
+                    Debug.DrawRay(linepos,master.rb.velocity, Color.red);
+                    Debug.DrawRay(linepos,transform.TransformDirection(Vector3.forward), Color.green);
+                    
+                    //Debug.DrawRay(linepos,master.transform.TransformDirection(localVelocity), Color.yellow);
+                    Debug.DrawRay(linepos,master.transform.TransformDirection(-localVelocity)*turnForceMultiplier, Color.blue);
+                }
+            }
+
+            /*
+
+            if (!Car.ApproximatelyT(master.rb.velocity.x,0,0.01f) || !Car.ApproximatelyT(master.rb.velocity.z,0,0.01f))
+            {
+                //float angleToVelocity = Vector3.SignedAngle(transform.forward,master.rb.velocity, Vector3.up);
+                Vector3 localVelocityZeroY = new Vector3(localVelocity.x,0,localVelocity.z);
+                float angleToVelocity = Vector3.SignedAngle(Vector3.forward,localVelocityZeroY, Vector3.up);
+
+                // Make sure forward & backward are 0° & left/right 90°
+                float absAngleToVelocity = Mathf.Abs(angleToVelocity);
+                if ((absAngleToVelocity > 90 && absAngleToVelocity < 180) || (absAngleToVelocity > 270 && absAngleToVelocity < 360))
+                {
+                    angleToVelocity = (90 - (absAngleToVelocity % 90)) * Mathf.Sign(angleToVelocity);
+                }
+                
+                //angleToVelocity = angleToVelocity > 0 ? angleToVelocity + 90 : angleToVelocity - 90;
+                Vector3 counterVelocity = Quaternion.AngleAxis(angleToVelocity, Vector3.up) * -master.rb.velocity;//Quaternion.AngleAxis(angleToVelocity, Vector3.up) * -master.rb.velocity;//master.rb.velocity/Mathf.Cos(Mathf.Abs(angleToVelocity)*Mathf.Deg2Rad);
+                //counterVelocity *= -1;
+                //counterVelocity *= Mathf.Sin((Mathf.Abs(angleToVelocity) % 90)); // 90° = 1, 0° = 0
+                
+                ////counterVelocity = Vector3.ClampMagnitude(counterVelocity, master.rb.velocity.magnitude); // Counter force can't be bigger than Forward force! 
+                
+                if(!Car.ApproximatelyT(angleToVelocity,0,0.01f))
+                    master.rb.AddForceAtPosition(counterVelocity, transform.position);
+                
+                if (doDebug)
+                {
+                    angleToVelocity = angleToVelocity % 90; //TMP JUST FOR LOGGING
+                    
+                    Debug.Log(angleToVelocity + " "+ GetHashCode());
+
+                    if (angleToVelocity > 60 || angleToVelocity < -60)
+                    {
+                        Debug.Log(Vector3.forward + " " + localVelocityZeroY + " "+master.rb.velocity);
+                    }
+                    
+                    
+                    Vector3 linepos = transform.position;
+                    linepos.y += 2;ity, Color.green);
+                    
+                    Debug.DrawRay(linepos,transform.TransformDirection(master.rb.velocity), Color.red);
+                    Debug.DrawRay(linepos,transform.TransformDirection(Vector3.forward), Color.green);
+                    
+                    Debug.DrawRay(linepos,-localVelocity, Color.yellow);
+                }
             }*/
+        
+        }
+
+        public void ApplyForce(float strength)
+        {
+            //master.AddForce();
+            ApplyForce(strength,Vector3.forward);
+        }
+
+        void ApplyForce(float strength, Vector3 localDirection)
+        {
+            if (!IsGrounded())
+                return;
             
-            master.rb.AddForceAtPosition(-localVelocity, transform.TransformPoint(transform.localPosition));
-            
-            
-            /*float angleToVelocity = Vector3.Angle(localVelocity,new Vector3(0,0,1));
-            Vector3 counterVelocity = Quaternion.AngleAxis(angleToVelocity, Vector3.up) * (localVelocity * -1);//Quaternion.Euler(0, angleToVelocity, 0) * localVelocity * -1;
-            //Debug.Log(""+angleToVelocity+"  "+master.rb.velocity+" "+master.rb.velocity.magnitude + "   "+counterVelocity);
-            ApplyForce(1,counterVelocity/*-localVelocity * (angleToVelocity/180)#1#);*/
-            
-            //ApplyForce(1,-localVelocity);
+            Vector3 force = transform.TransformDirection(localDirection) * strength;
+            force.y = 0; // HACK
+            master.rb.AddForceAtPosition(force,transform.position);
         }
         
-    }
+        private void HandleFloating()
+        {
+            RaycastHit hit;
+            Debug.DrawRay(transform.position, currentDistanceToGround*-3 * transform.up, Color.blue);
+            if (Physics.Raycast(transform.position, transform.up*-1, out hit, 100)) // TODO do raycast from bottom/exhaust
+            {
+                currentDistanceToGround = hit.distance;
+            }
+            else
+            {
+                currentDistanceToGround = float.PositiveInfinity; //zeroForceHeight;
+            }
+            float curveValue = Mathf.Clamp(currentDistanceToGround, 0, zeroForceHeight) / zeroForceHeight;
+            Vector3 finalForce = transform.TransformDirection(forceHeightCurve.Evaluate(curveValue) * maxForce);
+        
+            master.rb.AddForceAtPosition(finalForce, transform.position);
+        }
 
-    public void ApplyForce(float strength)
-    {
-        //master.AddForce();
-        ApplyForce(strength,new Vector3(0,0,1));
-    }
-
-    void ApplyForce(float strength, Vector3 localDirection)
-    {
-        Vector3 force = transform.TransformDirection(localDirection) * strength;
-        force.y = 0; // HACK
-        master.rb.AddForceAtPosition(force,master.transform.position + transform.localPosition);
+        bool IsGrounded()
+        {
+            return currentDistanceToGround <= distanceToGround;
+        }
     }
 }
