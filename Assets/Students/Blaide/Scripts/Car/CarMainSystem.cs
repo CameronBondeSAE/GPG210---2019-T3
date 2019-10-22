7b409b243eb7bf30da09e4d22b0a5e3d3398b592
Blaide <1017007@student.sae.edu.au>
2019-10-22 17:07:52 +1100

    Added Car flip, Acceleration curve, and added new Input System.

--- Assets/Students/Blaide/Scripts/Car/CarMainSystem.cs
+++ Assets/Students/Blaide/Scripts/Car/CarMainSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CarMainSystem : MonoBehaviour
 {
+    public inpu
     public float baseEngineTorque;
     public AnimationCurve springCurve;
     public List<WheelScript> wheels;
@@ -13,8 +14,14 @@ public class CarMainSystem : MonoBehaviour
     public float accelerator;
     public float steering;
     public AnimationCurve tyreFriction;
+    public float revs;
+    public float maxRevs;
+    public float revDrop;
+    public float accelerationMultiplier;
+    public AnimationCurve accelerationCurve;
     public float breaking;
     private Rigidbody rB;
+    public Transform centreOfMass;
 
 
     public float velocity;
@@ -22,6 +29,7 @@ public class CarMainSystem : MonoBehaviour
     void Start()
     {
         rB = GetComponent<Rigidbody>();
+        rB.centerOfMass.Set(centreOfMass.localPosition.x,centreOfMass.localPosition.y,centreOfMass.localPosition.z);
     }
 
     // Update is called once per frame
@@ -35,9 +43,33 @@ public class CarMainSystem : MonoBehaviour
 
     void FixedUpdate()
     {
+
+        if (accelerator != 0f && revs < maxRevs)
+        {
+            revs += accelerator * accelerationMultiplier;
+        }
+        else if (revs > 0)
+        {
+            revs -= revDrop;
+        }
+        else
+        {
+            revs = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            if (IsUpsideDown())
+            {
+              FlipCar();  
+            }
+
+            Debug.Log("up.y:" + transform.up.y);
+        }
+
+
         foreach ( WheelScript wheel in wheels)
         {
-            
             if (wheel.steeringWheel)
             {
                 wheel.transform.localRotation = Quaternion.Euler(steering * (wheel.invertSteering?-1:1) + 90 ,-90,270); 
@@ -52,16 +84,24 @@ public class CarMainSystem : MonoBehaviour
                 rB.AddForceAtPosition(wheel.transform.up * forceMultiplier * springCurve.Evaluate(hit.distance), wheel.transform.position);
                 wheel.wheelModel.transform.position = hit.point + Vector3.up *0.4f;
 
+
                 if (wheel.driveWheel)
                 {
-                  rB.AddForceAtPosition(wheel.transform.forward * accelerator * (baseEngineTorque/DriveWheels()),wheel.transform.position);  
+                  rB.AddForceAtPosition(wheel.transform.forward* accelerationCurve.Evaluate(revs) * (baseEngineTorque/DriveWheels()),wheel.transform.position);  
                 }
                 
                 
-                //asymetric friction
+                //asymmetric friction
                // Vector3 localVelocity = wheel.transform.InverseTransformDirection(rB.velocity);
+               
                Vector3 localVelocity = wheel.transform.InverseTransformDirection(wheel.transform.position - wheel.LastPosition)/ Time.deltaTime;
+               
                 rB.AddForceAtPosition (wheel.transform.TransformDirection(new Vector3(-localVelocity.x *tyreFriction.Evaluate(Mathf.Abs(localVelocity.x)),0,-localVelocity.z *tyreFriction.Evaluate(Mathf.Abs(localVelocity.z)) * breaking))  * 0.8f ,wheel.transform.position);
+            }
+            else
+            {
+                wheel.wheelModel.transform.position = wheel.transform.position +  wheel.transform.up * -(maxDistance - 0.4f);
+
             }
 
 
@@ -82,4 +122,18 @@ public class CarMainSystem : MonoBehaviour
 
         return i;
     }
+
+    private bool IsUpsideDown()
+    {
+        return transform.forward.y < -0.5;
+    }
+
+    private void FlipCar()
+    {
+        rB.velocity = new Vector3(0,0,0);
+        rB.angularVelocity = new Vector3(0,0,0);
+        transform.position += new Vector3(0, 5, 0);
+        transform.rotation = Quaternion.Euler(-90,0,0);
+    }
+
 }
