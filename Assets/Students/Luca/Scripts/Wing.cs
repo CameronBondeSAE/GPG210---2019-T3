c645a39d53c12d6f8d82f383c9bc2ed5f8b5d9b6
Luca <lucatannler@hotmail.com>
2019-10-28 00:28:35 +1100

    Helicoper Update: Rotor/Swash Plate Improvements
    - Minor improvements in the Swash Plates
    - Rearranged Rigidbody weights

--- Assets/Students/Luca/Scripts/Wing.cs
+++ Assets/Students/Luca/Scripts/Wing.cs
@@ -53,6 +53,24 @@ namespace Students.Luca
         private Quaternion maxForwardRotation; // Up
         private Quaternion maxBackwardRotation; // Down
 
+
+        public float angleToVelocity = 0;
+        public float angleToVelocityMultiplier = 0;
+        
+        public float angleForceToGround = 0;
+        public float angleForceToGroundMultiplier = 0;
+
+        // TEST VALUES FOR ANGULAR VELOCITY/ROTOR BLADES
+        public float airDensity = 1.1644f; // https://en.wikipedia.org/wiki/Density_of_air#Dry_air 30°C 1.1644
+        public float rotorArea = 3.5f; // Could be calculated by its actual mesh size; in m^2 
+        public float rotorRadius = 7; // Fictional length of the rotor; will be used to calculate lift forces at specific points
+        public float numberOfAttackPoints = 5; // Defines how many "Attack"-points (/Positions which generate upforce) there should be (Since we don't want to have one for every pixel like in reality)
+        public Rigidbody rotorRb;
+        
+        [ShowInInspector]
+        private float actualRotorRadius = 1; // Length of the rotor mesh
+        
+        
         // Start is called before the first frame update
         void Start()
         {
@@ -70,6 +88,22 @@ namespace Students.Luca
             if (masterRb == null)
             {
                 masterRb = GetComponentInParent<Rigidbody>();
+            }
+            
+            // Angular WIng style stuff
+            if (useAngularVelocity)
+            {
+                Collider rotorCollider = GetComponent<Collider>();
+                if (rotorCollider != null)
+                {
+                    actualRotorRadius = Mathf.Abs(transform.InverseTransformDirection(rotorCollider.bounds.size).x);
+                }
+                else
+                {
+                    
+                    actualRotorRadius = Mathf.Abs(transform.InverseTransformDirection(GetComponent<Mesh>()?.bounds.size ?? Vector3.one).x);
+                }
+                
             }
             
             // Debug stuff
@@ -116,17 +150,6 @@ namespace Students.Luca
             ApplyAirForces();
 
         }
-
-        public float angleToVelocity = 0;
-        public float angleToVelocityMultiplier = 0;
-        
-        public float angleForceToGround = 0;
-        public float angleForceToGroundMultiplier = 0;
-
-        // TEST VALUES FOR ANGULAR VEOLCITY
-        public float airDensity = 1.1644f; // https://en.wikipedia.org/wiki/Density_of_air#Dry_air 30°C 1.1644
-        public float rotorArea = 3.5f; // Could be calculated by its actual mesh size; in m^2 
-        public float rotorRadius = 7;
         
         private void ApplyAirForces()
         {
@@ -164,13 +187,39 @@ namespace Students.Luca
                 // L = CI * (r * V^2)/2 * A -> Lift = coefficient * (density * velocity^2)/2 * wing area
                 // ==> Simplified: .5 * CI * r * V^2 * A
 
-                Vector3 anglularVelocityY = masterRb.angularVelocity;
-                anglularVelocityY.x = 0;
-                anglularVelocityY.z = 0;
+                Vector3 angularVelocityY = rotorRb?.angularVelocity ?? Vector3.zero;
+                angularVelocityY.x = 0;
+                angularVelocityY.z = 0;
+                angularVelocityY.y = float.IsNaN(angularVelocityY.y) ? 0 : angularVelocityY.y;
 
-                float angleOfAttack = Vector3.Angle(transform.forward, Vector3.zero)*Mathf.Deg2Rad;
+                float localBladeAngle = transform.localRotation.eulerAngles.x % 360;
+                localBladeAngle = Mathf.Abs(localBladeAngle > 180 ? localBladeAngle - 360 : localBladeAngle); // Limit Angle to 180°
+                float angleOfAttack = /*Vector3.Angle(transform.forward, new Vector3(transform.forward.x,0,transform.forward.z))*/localBladeAngle*Mathf.Deg2Rad;
+                
                 // Coefficient CI: 2 * PI * Angle (Angle of Attack in Radians!)
                 float CI = 2 * Mathf.PI * angleOfAttack;
+
+                float xVirtualPositionSteps = rotorRadius / (numberOfAttackPoints+1); // +1 Because we don't count the rotor position as an attack point (Since the up lift is 0 there anyways)
+                float xActualPositionSteps = actualRotorRadius / (numberOfAttackPoints+1);
+                for (int i = 1; i <= numberOfAttackPoints; i++) // Info: Purposely starting at i=1 since we don't have to calculate the 0-point/Rotor position
+                {
+                    // Velocity at Tip
+                    float velocitySquared = Mathf.Pow(i*xVirtualPositionSteps * angularVelocityY.magnitude,2); // Distance from Rotor (Not accurate in this model) times the angular velocity of the rotor
+                    
+                    // Lift Force
+                    Vector3 liftForce = .5f * CI * airDensity * rotorArea * velocitySquared * transform.up;
+                    Vector3 forcePos = Vector3.right * ((actualRotorRadius/2) - i*xActualPositionSteps); // Requires pivot to be in the center; Right mus point to rotor Hacky
+                    masterRb.AddForceAtPosition(liftForce, transform.TransformPoint(forcePos));
+                    
+                    if (doDebug)
+                    {
+                        Debug.Log(liftForce+" at pos "+transform.TransformPoint(forcePos) + " "+CI+" "+velocitySquared + " " + localBladeAngle);
+                        Debug.DrawRay(transform.TransformPoint(forcePos),
+                            liftForce, Color.green);
+                    }
+                }
+                /*
+
                 
                 // Velocity at Tip
                 Vector3 vTip = rotorRadius * anglularVelocityY;
@@ -182,13 +231,13 @@ namespace Students.Luca
                 finalForce = .5f * CI * rotorRadius * vTipSquared * rotorArea;
                 //Debug.Log(finalForce+ "  "+transform.localRotation.eulerAngles.x+ " vTip^2: "+vTipSquared+" CL: "+CI+" RotRad: "+rotorRadius+" angVel: "+anglularVelocityY);
                 Vector3 tipPos = Vector3.right * -(rotorRadius/2); // If Pivot in center....Hacky anyways
-                //masterRb.AddForceAtPosition(finalForce, transform.TransformPoint(tipPos));
+                masterRb.AddForceAtPosition(finalForce, transform.TransformPoint(tipPos));
                 
                 if (doDebug)
                 {
                     Debug.DrawRay(transform.TransformPoint(tipPos),
                         finalForce, Color.green);
-                }
+                }*/
             }
             
         }
