bfc1f41f31097e3afd42e3ba49a06f61fab6f9e7
Unknown <Dylanwebb5@hotmail.com>
2019-11-05 19:01:47 +1100

    Updated Plane

--- Assets/Students/Dylan/DylanPlane.cs
+++ Assets/Students/Dylan/DylanPlane.cs
@@ -10,6 +10,8 @@ public class DylanPlane : MonoBehaviour
 
     private Rigidbody rb;
 
+    private Quaternion AddRot;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,16 +20,26 @@ public class DylanPlane : MonoBehaviour
     void FixedUpdate()
     {
         Quaternion AddRot = Quaternion.identity;
+
         float roll = 0;
         float pitch = 0;
         float yaw = 0;
+
         roll = Input.GetAxis("Roll") * (Time.deltaTime * RotationSpeed);
         pitch = Input.GetAxis("Pitch") * (Time.deltaTime * RotationSpeed);
         yaw = Input.GetAxis("Yaw") * (Time.deltaTime * RotationSpeed);
-        AddRot.eulerAngles = new Vector3(-pitch, yaw, -roll);
-        GetComponent<Rigidbody>().rotation *= AddRot;
+
+        RotatePlane(pitch, yaw, roll);
+        
         Vector3 AddPos = Vector3.forward;
         AddPos = rb.rotation * AddPos;
-        GetComponent<Rigidbody>().velocity = AddPos * (Time.deltaTime * AmbientSpeed);
+        rb.velocity = AddPos * (Time.deltaTime * AmbientSpeed);
     }
+
+    void RotatePlane(float pitch, float yaw, float roll)
+    {
+        AddRot.eulerAngles = new Vector3(pitch, yaw, roll);
+        rb.rotation *= AddRot;
+    }
+
 }
