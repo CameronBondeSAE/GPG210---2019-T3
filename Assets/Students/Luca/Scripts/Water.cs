b5c29a6f85569477be51d7f0675659b5c0a80faf
Luca <lucatannler@hotmail.com>
2019-11-19 17:09:36 +1100

    Water/Wave Playing around/Testing
    Playing around with water, trying to get waves together with cloth physics. Nothing really achieved.

--- Assets/Students/Luca/Scripts/Water.cs
+++ Assets/Students/Luca/Scripts/Water.cs
@@ -7,6 +7,11 @@ namespace Students.Luca.Scripts
 {
     public class Water : MonoBehaviour
     {
+        public MeshCollider meshCollider;
+        public MeshFilter meshFilter;
+        public SkinnedMeshRenderer skinnedMeshRenderer;
+        public Cloth cloth;
+        
         public float density = 1025;
 
         [ShowInInspector, SerializeField]
@@ -39,11 +44,52 @@ namespace Students.Luca.Scripts
 
         private void Start()
         {
+            if (meshCollider == null)
+            {
+                meshCollider = GetComponent<MeshCollider>();
+            }
+            if (meshFilter == null)
+            {
+                meshFilter = GetComponent<MeshFilter>();
+            }
+
+            if (cloth == null)
+            {
+                cloth = GetComponent<Cloth>();
+            }
+
+            if (skinnedMeshRenderer == null)
+            {
+                skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+            }
+            
             waterCloth = GetComponent<Cloth>();
             //waveTween = transform.DOPunchPosition(new Vector3(0, HeightIntensity, 0), DurationIntensity, 10, 1f, false);
             //transform.DOMoveY(heightIntensity, time);
             //waveTween = DOTween.Punch(() => transform.position, (x) => transform.position = x, new Vector3(0,heightIntensity,0), durationIntensity, 10, 1f).SetLoops(-1,LoopType.Yoyo).Play();
             waveTween = DOTween.To(() => transform.position, (x) => transform.position = x, transform.position+(new Vector3(0,heightIntensity,0)), durationIntensity).SetLoops(-1,LoopType.Yoyo);
+        }
+
+        private void Update()
+        {
+            if (HeightIntensity > 0 && cloth != null && meshCollider != null)
+            {
+                //meshCollider.sharedMesh = null;
+                
+                /*Mesh m = skinnedMeshRenderer.sharedMesh;
+                m.vertices = GetComponent<Cloth>().vertices;
+                m.RecalculateNormals();*/
+                /*Mesh m = new Mesh();
+                skinnedMeshRenderer.BakeMesh(m);*/
+                /*meshCollider.sharedMesh.vertices = cloth.vertices;
+                meshCollider.sharedMesh.RecalculateNormals();
+                meshCollider.sharedMesh.RecalculateBounds();
+                meshCollider.sharedMesh.RecalculateTangents();*/
+
+                //meshCollider.sharedMesh = meshFilter.sharedMesh;
+
+
+            }
         }
 
         private void HandleWaveIntensityChanged()
