925b6fd0172b614b4b4dbcef0967faf1de1ba4dd
Unknown <Dylanwebb5@hotmail.com>
2019-11-19 12:45:49 +1100

    Updated Scripts
    - Added in Fuel Component
    - Tested UI still not working

---
+++ Assets/Students/Dylan/Scripts/FuelComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelComponent : MonoBehaviour
+{
+    public float currentFuel;
+    public float maxFuel;
+
+    public int fuelDrainRate;
+
+    //not each vehicle just references this bool and if its true then take no input
+    public bool outOfFuel;
+
+    public void Update()
+    {
+        if (currentFuel <= 0)
+        {
+            outOfFuel = true;
+        }
+        else
+        {
+            outOfFuel = false;
+        }
+    }
+
+    //each vehicle references this function and calls it everytime there is input from player
+    public void DrainFuel(float fuel)
+    {
+        currentFuel -= fuelDrainRate;
+    }
+}
--- Assets/Students/Dylan/Scripts/UI.cs
+++ Assets/Students/Dylan/Scripts/UI.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 public class UI : MonoBehaviour
 {
     public float fuel;
     public float maxFuel;
     //public Text fuelText;
-
+    private FuelComponent fuelComponent;
 
     private void Start()
     {
-
+        fuelComponent = FindObjectOfType<FuelComponent>();
         //the below is getting a reference to the max fuel from the fuel component script
-        //maxFuel = GetComponent<FuelComponent>().MaxFuel;
+        maxFuel = fuelComponent.maxFuel;
         //fuelText.text = "Fuel: " + fuel.ToString("F2");
     }
 
     private void Update()
     {
+        fuel = fuelComponent.currentFuel;
         //fuelText.text = "Fuel: " + fuel.ToString("F2");
 
         if (fuel >= 0)
