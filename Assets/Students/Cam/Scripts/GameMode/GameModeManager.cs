ccdc79cb7daa0268d1eb9cb769ab53ab449c0540
Cameron Bonde <CameronBonde@users.noreply.github.com>
2019-11-27 17:16:53 +1100

    Gamemode init

---
+++ Assets/Students/Cam/Scripts/GameMode/GameModeManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cam
+{
+    public class GameModeManager : MonoBehaviour
+    {
+//    public LevelManager LevelManager;
+        public GameModeBase GameMode;
+
+        public event Action OnAnother;
+
+        public delegate void DoThingDelegate();
+        public event DoThingDelegate OnDoThing;
+
+
+        public event Action<int> OnAnotherWithInt;
+
+        public delegate void DoAnotherWithInt(int stuff);
+        public event DoAnotherWithInt OnDoAnotherWithInt;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+//        LevelManager.LevelStart();
+            GameMode.Activate();
+            OnDoThing?.Invoke();
+            OnAnotherWithInt?.Invoke(5);
+        }
+    }
+}
