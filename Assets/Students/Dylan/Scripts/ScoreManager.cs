185ae7dcb8f49f7023fadc4bba53b1de1d79e4f9
Unknown <Dylanwebb5@hotmail.com>
2019-12-02 16:16:12 +1100

    Score Manager (Beta)
    - Adjusted Camera To be turned off
    - Score manager has do tween making score text bigger and then disappears

--- Assets/Students/Dylan/Scripts/ScoreManager.cs
+++ Assets/Students/Dylan/Scripts/ScoreManager.cs
@@ -1,29 +1,70 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     
-    public List<int> playerPosition = new List<int>();
+    private List<int> playerPosition = new List<int>();
     //private CheckpointReachedPlayerData checkPointData;
 
-    public Text playerScore;
+    private  Text playerScore;
 
-    public List<Text> playerScores = new List<Text>();
+    private List<Text> playerScores = new List<Text>();
+
+    private float scoreToTween = 1f;
+    private float tweenDuration = 2f;
     
+    public Text scoreText;
+
+    private void Awake()
+    {
+        ScoreManagerInit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TweenScore(1);
+        }
+
+        scoreText.text = scoreToTween.ToString("F0");
+    }
 
     public void ScoreManagerInit()
     {
         //make player scores the same size as player positions 
-        
+
+        scoreText.gameObject.SetActive(false);
     }
-    private void Update()
+    
+    public void TweenScore(float scoreToIncrease)
     {
         
+        scoreText.transform.localScale = new Vector3(1, 1, 1);
+        scoreText.gameObject.SetActive(true);
+        DOTween.To(Getter, Setter,scoreToIncrease,tweenDuration).OnComplete(MakeTextTransparent);
+    }
 
+    private void Setter(float value)
+    {
+        scoreToTween = value;
+
+        scoreText.transform.localScale = new Vector3(value, value, 1);
+    }
+
+    private float Getter()
+    {
+        return scoreToTween;
+    }
+
+    private void MakeTextTransparent()
+    {
+        scoreText.gameObject.SetActive(false);
     }
 
     public void FinalScoreBoard()
@@ -35,7 +76,7 @@ public class ScoreManager : MonoBehaviour
         }
     }
     
-    //TODO figure out why list isn't sorting properly
+    
     private void SortScoreBoard()
     {
         playerPosition.Sort(SortList);
@@ -44,13 +85,13 @@ public class ScoreManager : MonoBehaviour
     {
         return a.CompareTo(b);
     }
-
+    /*
     private GameObject CreateText()
     {
         GameObject scoreText = new GameObject("ScoreText");
         scoreText.transform.SetParent(this.transform);
         scoreText .GetComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
         return scoreText;
-    }
+    }*/
 
 }
