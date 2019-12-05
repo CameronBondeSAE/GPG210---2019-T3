36be0cf20f0c3e02d7723a02218af36baaf45a67
Unknown <Dylanwebb5@hotmail.com>
2019-12-05 13:11:26 +1100

    UI Work
    Started work on UI names

---
+++ Assets/Students/Dylan/Scripts/PlayerNameUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameUI : MonoBehaviour
+{
+    public string playerName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        playerName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome " + playerName;
+        //return playerName;
+    }
+
+    public void GiveNameToPlayerInfos(PlayerInfo playerInfo)
+    {
+        //playerInfo.name = playerName;
+    }
+}
