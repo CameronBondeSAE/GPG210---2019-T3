b1a50591efce47ef8d85353acf6aa7504a28c4d0
Blaide <1017007@student.sae.edu.au>
2019-12-07 23:36:43 +1100

    +SpawnerConditions +player maxVelocity + player floats on water
    Added boat spawner, 
    trees no longer spawn under water.
    vehicles spawn aligned with the terrain based on hit normal.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
 
 public class Spawner : MonoBehaviour
 {
+    public LayerMask layerMask;
     public BoxCollider boundingBox;
-
     public GameObject[] prefabs;
     public int number;
     public float offsetFromGround = 5f;
     public float maxRandomRotation = 360;
+    public enum SpawnCondition
+    {
+        SpawnOnLand,
+        SpawnOnWater,
+        SpawnUnderWater
+    }
+    public List<SpawnCondition> spawnConditions;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +30,24 @@ public class Spawner : MonoBehaviour
 
             Ray ray = new Ray(new Vector3(pointInBounds.x, boundingBox.bounds.max.y, pointInBounds.z), Vector3.down);
             RaycastHit hitInfo;
-            Physics.Raycast(ray, out hitInfo, boundingBox.bounds.size.y);
-            pointInBounds.y = hitInfo.point.y + offsetFromGround; // TODO: Should read normal or do corner checks
-
+            Physics.Raycast(ray, out hitInfo, boundingBox.bounds.size.y,~layerMask, ( !spawnConditions.Contains(SpawnCondition.SpawnUnderWater) ? QueryTriggerInteraction.Collide : QueryTriggerInteraction.Ignore ));
+            pointInBounds.y = hitInfo.point.y + offsetFromGround; // DONE: Should read normal or do corner checks 
             
-            Instantiate(prefabs[Random.Range(0, prefabs.Length)], pointInBounds,
-                Quaternion.Euler(0, Random.Range(0, maxRandomRotation), 0));
+            /*Instantiate(prefabs[Random.Range(0, prefabs.Length)], pointInBounds,
+                Quaternion.Euler(0, Random.Range(0, maxRandomRotation), 0));*/
+
+            if (!(!spawnConditions.Contains(SpawnCondition.SpawnOnLand) &&
+                  hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("terrain"))
+                && !(!spawnConditions.Contains(SpawnCondition.SpawnOnWater) &&
+                     (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("Water"))))
+            {
+                Quaternion rot = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+                Instantiate(prefabs[Random.Range(0, prefabs.Length)], pointInBounds,rot);
+            }
+            else
+            {
+                i--;
+            }
         }
     }
 
--- Assets/Students/Blaide/Scripts/FPS/PlayerMovement.cs
+++ Assets/Students/Blaide/Scripts/FPS/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Students.Luca.Scripts;
 using UnityEngine;
 using UnityEngine.InputSystem.HID;
 
@@ -15,7 +16,10 @@ namespace Students.Blaide
         public LayerMask JumpOffAble;
         public float speed;
         public float maxVelocity;
-        
+        public Water water;
+        public bool grounded;
+        public bool inWater;
+        public float submergence;
         private Vector3 velocity;
         private Vector3 moveDir = Vector3.zero;
         public Vector2 leftStickBuffer;
@@ -24,6 +28,7 @@ namespace Students.Blaide
         private bool JumpPressed;
 
         public float jumpForce;
+        public float bouyancy;
 
         void Start()
         {
@@ -60,44 +65,86 @@ namespace Students.Blaide
             moveDir.x = leftStickBuffer.x;
             moveDir.y = 0;
             moveDir = transform.rotation * moveDir;
-            moveDir *= (IsGrounded() ? 1 : 0.2f) * speed * Time.deltaTime;
-            if (IsGrounded())
+            
+            
+            RaycastHit hit;
+            Vector3 rayDirection = feetPos.InverseTransformDirection(Vector3.down);
+            Vector3 rayOrigin = feetPos.position;
+            grounded = (Physics.Raycast(rayOrigin, rayDirection, out hit, 0.3f, JumpOffAble,QueryTriggerInteraction.Collide));
+            
+            moveDir *= (grounded ? 1 : 0.2f) * speed * Time.deltaTime;
+            
+            
+            if (grounded)
+            {
+                if (hit.collider.GetComponent<Water>()!= null)
+                {
+                    water = hit.collider.GetComponent<Water>();
+                }
+            }
+
+           
+
+            inWater = false;
+            if (water != null)
+            {
+                submergence = water.GetSurfaceLevelAtPos(feetPos.position.x, feetPos.position.y) -feetPos.position.y;
+                if ((water.GetComponent<Collider>().bounds.Contains(feetPos.position))&& submergence >= 0.01f)
+                {
+                    inWater=true;
+                }
+               
+            }
+
+
+
+
+
+            if (grounded && !inWater)
             {
                 if ( JumpPressed )
                {
                    rB.AddForce(Vector3.up *jumpForce);
                }
+                moveDir = Vector3.ProjectOnPlane(moveDir, hit.normal);
             }
-            JumpPressed = false;
-            moveDir = Vector3.ProjectOnPlane(moveDir, Ground().normal);
+            else if(inWater)
+            {
+                //if (JumpPressed)
+                //{
+                    rB.AddForce(Vector3.up *bouyancy * submergence/2);
+               // }
+            }
+
+            if (!inWater)
+            {
+                JumpPressed = false;
+            }
+
+            
             
             rB.AddForce(moveDir);
             
-           // Debug.Log("moveDir.z = " + moveDir.z + ". moveDir.x = " + moveDir.x + ". moveDir.y = "+ moveDir.y + ". JumpForce = " + jumpForce + ".");
-        }
+            if (rB.velocity.magnitude > maxVelocity)
+            {
+                rB.AddForce(rB.velocity.normalized*(maxVelocity- rB.velocity.magnitude));
+            }
 
-        public bool IsGrounded()
-        {
-            RaycastHit hit;
-            Vector3 rayDirection = feetPos.InverseTransformDirection(Vector3.down);
-            Vector3 rayOrigin = feetPos.position;
-            bool didHit = (Physics.Raycast(rayOrigin, rayDirection, out hit, 0.3f, JumpOffAble));
-            return didHit;
+            
+            
         }
-
-        public RaycastHit Ground()
-        {
-            RaycastHit hit;
-            Vector3 rayDirection = feetPos.InverseTransformDirection(Vector3.down);
-            Vector3 rayOrigin = feetPos.position;
-            Physics.Raycast(rayOrigin, rayDirection, out hit, 0.3f, JumpOffAble);
-            return hit;
-        }
-
         private void OnDrawGizmos()
         {
             Gizmos.DrawLine(feetPos.position,feetPos.position + feetPos.InverseTransformDirection(Vector3.down)*0.3f);
         }
+/*        private void OnTriggerEnter(Collider col)
+        {
+            var w = col.GetComponent<Water>();
+            if (w != null)
+            {
+                water = w;
+            }
+        }*/
     } 
 }
 
